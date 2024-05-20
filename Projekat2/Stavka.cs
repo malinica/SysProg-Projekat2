44c3209df438ad72099a6c149faad1c784810988
unknown <malinovicjovana13@gmail.com>
2024-05-20 13:50:58 +0200

    Dodata klasa Stavka

---
+++ Projekat2/Stavka.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Projekat1
+{
+    public class Stavka
+    {
+        public int Ukupno { get; set; }
+        public string Podaci { get; set; }
+        public DateTime VremeKreiranja { get; set; }
+
+        public Stavka()
+        {
+            VremeKreiranja = DateTime.UtcNow;
+        }
+        public Stavka(int u, string p)
+        {
+            Ukupno = u;
+            Podaci = p;
+            VremeKreiranja = DateTime.UtcNow;
+        }
+        public override string ToString()
+        {
+            return $" kreirano: {VremeKreiranja}, ima ukupno {Ukupno} podataka: {Podaci}";
+        }
+    }
+}
